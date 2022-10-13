a0ba2ef046f06ae5b9f79e7c3f7051ff646ce5de
mohamed.sallam <mohamed.sallam@SW-M-SALLAM>
2022-10-13 08:25:54 +0200

    Add project files.

---
+++ AdminDashboard/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using AdminDashboard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminDashboard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
